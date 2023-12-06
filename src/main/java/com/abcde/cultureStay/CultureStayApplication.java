7965c2a215d772cd926ca8be2bee417ff0e2f80b
sincheon90 <sincheon90@gmail.com>
2023-12-06 15:11:03 +0900

    1206 프로젝트 다시생성

---
+++ src/main/java/com/abcde/cultureStay/CultureStayApplication.java
@@ -0,0 +1,13 @@
+package com.abcde.cultureStay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureStayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abcde/cultureStay/CultureStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcde.cultureStay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
