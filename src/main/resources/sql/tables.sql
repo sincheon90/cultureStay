219fd4534c21dbe51a65fe91d54d37f6a588ea72
Park Juni <juneya10@naver.com>
2023-12-12 17:57:10 +0900

    업데이트 SQL
    tables.sql

--- src/main/resources/sql/tables.sql
+++ src/main/resources/sql/tables.sql
@@ -1,111 +1,113 @@
-CREATE TABLE "User" (
-	"userid"	NOT NULL		NOT NULL,
-	"password"	NOT NULL		NULL,
-	"username"	NOT NULL		NULL,
-	"phone"	not null		NULL,
-	"address"	NOT NULL		NULL,
-	"email"	NOT NULL		NULL,
-	"birth"	NOT NULL		NULL,
-	"profile_picture"	NULL		NULL,
-	"Field2"	일반		NULL,
-	"Field"	NOT NULL		NULL
+--유저테이블
+CREATE TABLE Users(
+	userid	    varchar2(255)	    primary key,   
+	password	varchar2(255)	    not null,  
+	username	varchar2(255)	    not null,  
+	phone	    varchar2(255)		not null,  
+	address 	varchar2(255)		NULL,           
+	email	    varchar2(100)		not null,        --이메일
+	birth	    date		        NULL,            --생년월일
+    gender      varchar2(20)        not null,        --성별
+	profilePic	varchar2(255)	    NULL,        --프사
+    verified    varchar2(1) CHECK(verified IN  ('0','1'))--본인인증 여부
+); --verified 디폴트값 넣어주기
+
+--유저 좋아요,북마크, 최근방문 프로그램 :  추천시 사용
+CREATE TABLE Interests(     
+    	userid	    varchar2(255)	    primary key references Users(userid), 
+        favorite     number                 null,          --좋아요한 프로그램 아이디(어레이리스트)
+        bookmark    number                 null,         --북마크한 프로그램 아이디(어레이리스트)
+        recentClick number                 null         --최근 클릭한 프로그램
 );
 
-CREATE TABLE "Program" (
-	"programNum"	VARCHAR(255)		NOT NULL,
-	"userid"	NOT NULL		NOT NULL,
-	"title"	NOT NULL		NULL,
-	"content"	NOT NULL		NULL,
-	"address"	NOT NULL		NULL,
-	"price"	not null		NULL,
-	"start_date"	NOT NULL		NULL,
-	"end_date"	NOT NULL		NULL,
-	"Field"	NOT NULL		NULL,
-	"Field2"	NULL		NULL
+CREATE TABLE Program (
+	programNum	number	            primary key,
+	userid	    varchar2(255)	references Users(userid), 
+	title	    varchar2(255)	NOT NULL,
+	content	    varchar2(4000)	NOT NULL,
+	address	    varchar2(255)	NOT NULL,
+	price	    number		        NOT NULL,
+    tag         number             NOT NULL,
+	start_date	date		    NOT NULL,
+	end_date	date		    NOT NULL,
+    hits            number              default 0 --인기프로그램용 조회수
+);
+create sequence programNum_seq;
+
+--인기태그용 태그클릭수
+create table tagClick_cnt(
+    one     number     default 0,
+    two      number     default 0,
+    three    number     default 0,
+    four     number     default 0,
+    five     number     default 0,
+    six      number     default 0,
+    seven    number     default 0,
+    eight    number     default 0,
+    nine    number     default 0,
+    ten      number     default 0
 );
 
-CREATE TABLE "Review" (
-	"Key"	NOT NULL		NOT NULL,
-	"programNum"	NOT NULL		NOT NULL,
-	"Key2"	NOT NULL		NOT NULL,
-	"Field"	NOT NULL		NULL,
-	"Field2"	NOT NULL		NULL,
-	"Field3"	NOT NULL		NULL,
-	"Field4"	NOT NULL		NULL,
-	"Field5"	NULL		NULL,
-	"Field6"	NOT NULL		NULL
-);
 
-CREATE TABLE "Chat" (
-	"Key"	NOT NULL		NOT NULL,
-	"programNum"	NOT NULL		NOT NULL,
-	"userid"	NOT NULL		NOT NULL,
-	"hostid"	NOT NULL		NULL,
-	"Field4"	NOT NULL		NULL,
-	"Field5"	NOT NULL		NULL,
-	"Field"	NOT NULL		NULL
-);
 
-CREATE TABLE "Reservation" (
-	"Key"	NOT NULL		NOT NULL,
-	"programNum"	NOT NULL		NOT NULL,
-	"userid"	NOT NULL		NOT NULL,
-	"Field3"	NOT NULL		NULL,
-	"Field4"	NOT NULL		NULL,
-	"Field5"	NOT NULL		NULL,
-	"Field6"	NOT NULL		NULL,
-	"Field7"	NULL		NULL
+CREATE TABLE Review (
+	reviewNum   	number		            primary key,
+	programNum	    number		            references Program(programNum), 
+	customerID	    varchar2(255)		references Users(userid), 
+	hostID	        varchar2(255)		references Users(userid), 
+	reviewerID	    varchar2(255)		references Users(userid), 
+	stars	        number	    	        NOT NULL,
+	content	        varchar2(2000)  	NOT NULL,
+	reviewPic	    varchar2(255)	    NULL,
+	start_date	    date		        NOT NULL
 );
+create sequence reviewNum_seq;
 
-CREATE TABLE "Board" (
-	"boardnum"	NOT NULL		NOT NULL,
-	"userid"	NOT NULL		NOT NULL,
-	"title"	NOT NULL		NULL,
-	"content"	NOT NULL		NULL,
-	"inputdate"	NOT NULL		NULL,
-	"hits"	0		NULL,
-	"files"	NULL		NULL,
-	"Field"	NOT NULL		NULL
-);
 
-CREATE TABLE "Reply" (
-	"replynum"	NOT NULL		NOT NULL,
-	"boardnum"	NOT NULL		NOT NULL,
-	"userid"	NOT NULL		NOT NULL,
-	"content"	NOT NULL		NULL,
-	"inputdate"	NOT NULL		NULL
-);
 
-CREATE TABLE "Wish" (
-	"userid"	NOT NULL		NOT NULL,
-	"programNum"	NOT NULL		NOT NULL
+CREATE TABLE Reservation (
+	reserNum	number		            primary key,
+	programNum	number		            references Program(programNum), 
+	userid	    varchar2(255)		references Users(userid),
+	start_date	date		    NOT NULL,
+	end_date	date		    NOT NULL,
+    request	    varchar2(255)		NULL, --요청사항
+	payment 	number		NULL,   --결제수단(0,1,2,3)
+	status	    number		default 0       --예약상태(0,1,2,3)
 );
+create sequence reserNum_seq;
 
-ALTER TABLE "User" ADD CONSTRAINT "PK_USER" PRIMARY KEY (
-	"userid"
+CREATE TABLE Board(
+    boardnum	    number		            primary key,
+	userid          varchar2(255)		references Users(userid),
+    title           varchar2(300)       not null,                               --제목
+    contents        varchar2(4000)      not null,                               --글내용
+    inputdate       date                default sysdate,     
+    hits            number              default 0,   
+    originalfile    varchar2(300),      --첨부파일의 원래이름 사진.jpg -> 20220727.jpg
+    savedfile       varchar2(100)       --첨부파일이 서버에 저장된 이름
 );
+create sequence boardnum_seq;
 
-ALTER TABLE "Program" ADD CONSTRAINT "PK_PROGRAM" PRIMARY KEY (
-	"programNum"
+CREATE TABLE Reply (
+	replynum      number		            primary key,
+	boardnum    number		references Board(boardnum),
+	userid	 varchar2(255)		references Users(userid),
+	content	varchar2(4000)      not null,     
+	inputdate date                default sysdate
 );
+create sequence replynum_seq;
 
-ALTER TABLE "Review" ADD CONSTRAINT "PK_REVIEW" PRIMARY KEY (
-	"Key"
-);
 
-ALTER TABLE "Chat" ADD CONSTRAINT "PK_CHAT" PRIMARY KEY (
-	"Key"
-);
 
-ALTER TABLE "Reservation" ADD CONSTRAINT "PK_RESERVATION" PRIMARY KEY (
-	"Key"
-);
+--https://velog.io/@hiy7030/TIL-%EC%B1%84%ED%8C%85-%EA%B8%B0%EB%8A%A5-ERD-%EC%84%A4%EA%B3%84
+--CREATE TABLE Chat (
+--	chatNum         number		            primary key,
+--	programNum	    number		            references Program(programNum), 
+--	userid	        varchar2(255)		references User(userid), 
+--	hostid	        varchar2(255)		references User(userid)
+--);
+--create sequence chatNum_seq;
 
-ALTER TABLE "Board" ADD CONSTRAINT "PK_BOARD" PRIMARY KEY (
-	"boardnum"
-);
 
-ALTER TABLE "Reply" ADD CONSTRAINT "PK_REPLY" PRIMARY KEY (
-	"replynum"
-);
 
