4de5cff6feb59a064c13d717c9cd05761394a173
sincheon90 <sincheon90@gmail.com>
2023-12-13 17:07:05 +0900

    translate 메소드 작성

--- src/main/java/com/abcde/cultureStay/util/TestMain.java
+++ src/main/java/com/abcde/cultureStay/util/TestMain.java
@@ -1,12 +1,6 @@
 package com.abcde.cultureStay.util;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.translate.Translate;
-import com.google.cloud.translate.TranslateOptions;
-import com.google.cloud.translate.Translation;
-import org.springframework.http.HttpHeaders;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import com.abcde.cultureStay.service.TranslateService;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -14,48 +8,13 @@ import java.io.IOException;
 public class TestMain {
 
     public static void main(String[] args) {
-        String credentialsPath = "C:/client_secret_527056499323-bqk3ngc2ck7ufsjj0re8bjog52cqf4co.apps.googleusercontent.com.json";
 
-        Translate translate = null;
+        TranslateService service = new TranslateService();
 
-        try {
-            // OAuth2 인증으로 GoogleCredentials를 로드합니다.
-            GoogleCredentials credentials = GoogleCredentials
-                    .fromStream(new FileInputStream(credentialsPath))
-                    .createScoped("https://www.googleapis.com/auth/cloud-platform");
+        String text = "헤이 거기 안녕!!";
 
-            // GoogleCredentials를 사용하여 TranslateOptions을 초기화합니다.
-            translate = TranslateOptions.newBuilder().setCredentials(credentials).build().getService();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Translate API를 사용하여 번역합니다.
-        if (translate != null) {
-            Translation translation = translate.translate(
-                    "Hola Mundo!",
-                    Translate.TranslateOption.sourceLanguage("es"),
-                    Translate.TranslateOption.targetLanguage("de"),
-                    Translate.TranslateOption.model("nmt")
-            );
-
-            System.out.printf("Translated Text:\nText: %s\n", translation.getTranslatedText());
-        }
-
-
-
-//        Translate translate = TranslateOptions.getDefaultInstance().getService();
-//
-//        Translation translation =
-//                translate.translate(
-//                        "Hola Mundo!",
-//                        Translate.TranslateOption.sourceLanguage("es"),
-//                        Translate.TranslateOption.targetLanguage("de"),
-//                        // Use "base" for standard edition, "nmt" for the premium model.
-//                        Translate.TranslateOption.model("nmt"));
-//
-//        System.out.printf("TranslatedText:\nText: %s\n", translation.getTranslatedText());
-
+        String result = service.translate("ko", "en", text);
+        System.out.println(result);
 
     }
 }
