fa2ec5204ec39dc784fd241be75a95eca5f41ee1
sincheon90 <sincheon90@gmail.com>
2024-01-09 09:32:50 +0900

    프로그램 이미지 추출하여 넣기 - 기본이미지 넣기

--- src/main/java/com/abcde/cultureStay/util/HtmlUtils.java
+++ src/main/java/com/abcde/cultureStay/util/HtmlUtils.java
@@ -6,6 +6,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.safety.Safelist;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,6 +16,12 @@ public class HtmlUtils {
 
     static int maxLength = 500;
     static int maxLines = 15;
+
+    @Value("${image.getImage.url}")
+    String url;
+    static String defaultImage1 = "/img/board/img.png";
+
+    static String defaultImage2 = "/img/board/img_5.png";
 
     public static String extractText(String html) {
         String text = Jsoup.clean(html, Safelist.none());
@@ -41,9 +48,22 @@ public class HtmlUtils {
     public static ArrayList<Program> extractPreview(ArrayList<Program> programList) {
         for (Program program : programList) {
             // 미리보기용 이미지 추출
-            ArrayList<String> list = extractTwoImages(program.getContent());
-            program.setImagePath1(list.get(0));
-            program.setImagePath2(list.get(1));
+            ArrayList<String> imageList = extractTwoImages(program.getContent());
+
+            // 이미지 리스트 검사 및 안전한 할당
+            if (!imageList.isEmpty()) {
+                program.setImagePath1(imageList.get(0));
+                if (imageList.size() > 1) {
+                    program.setImagePath2(imageList.get(1));
+                } else {
+                    // 이미지가 하나만 있는 경우 또는 대체할 기본 이미지 경로
+                    program.setImagePath2("defaultImagePath2");
+                }
+            } else {
+                // 이미지가 없는 경우 기본 이미지 경로 할당
+                program.setImagePath1(defaultImage1);
+                program.setImagePath2(defaultImage2);
+            }
 
             // 미리보기용 텍스트 추출
             program.setContent(extractText(program.getContent()));
