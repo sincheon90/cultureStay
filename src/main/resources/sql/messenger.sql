a471e724b28c2ba8fdde14f0131b40061215d553
sincheon <sinchon90@gmail.com>
2024-01-14 21:10:02 +0900

    메신저 sql

---
+++ src/main/resources/sql/messenger.sql
@@ -0,0 +1,40 @@
+-- Messages 테이블 생성
+CREATE TABLE Messages (
+    messageId NUMBER PRIMARY KEY,
+    chatRoomId NUMBER,
+    senderId VARCHAR2(255),
+    messageText VARCHAR2(4000),
+    timestamp DATE DEFAULT SYSDATE,
+    isRead NUMBER,
+    messageType VARCHAR2(255) -- 텍스트, 이미지 등
+);
+CREATE SEQUENCE Messages_seq;
+
+-- ChatRooms 테이블 생성
+CREATE TABLE ChatRooms (
+    chatRoomId NUMBER PRIMARY KEY,
+    chatRoomName VARCHAR2(255),
+    createTimestamp DATE DEFAULT SYSDATE,
+    createdUserId NUMBER
+);
+CREATE SEQUENCE ChatRooms_seq;
+
+-- ChatRoomMembers 테이블 생성
+CREATE TABLE ChatRoomMembers (
+    chatRoomMemberId NUMBER PRIMARY KEY,
+    chatRoomId NUMBER,
+    userId VARCHAR2(255),
+    joinedTimestamp DATE DEFAULT SYSDATE,
+    memberRole VARCHAR2(255)
+);
+CREATE SEQUENCE ChatRoomMembers_seq;
+
+
+-- Attachments 테이블 생성
+CREATE TABLE Attachments (
+    attachmentId NUMBER PRIMARY KEY,
+    messageId NUMBER,
+    filePath VARCHAR2(1000),
+    fileType VARCHAR2(255)
+);
+CREATE SEQUENCE Attachments_seq;
